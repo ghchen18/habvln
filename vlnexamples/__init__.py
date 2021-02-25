e3f5f70b4b8a55bd9f466b35509a7c633ceee395
ghchen18 <eric18@connect.hku.hk>
2021-02-25 16:49:02 +0800

    update

--- vlnexamples/__init__.py
+++ vlnexamples/__init__.py
@@ -4,9 +4,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .shortest_path_follower_example import SimpleRLEnv, draw_top_down_map
+# from .shortest_path_follower_example import SimpleRLEnv, draw_top_down_map
 
-__all__ = [
-    'SimpleRLEnv',
-    'draw_top_down_map',
-]
+# __all__ = [
+#     'SimpleRLEnv',
+#     'draw_top_down_map',
+# ]
